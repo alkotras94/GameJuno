05b842d58b3eb39f6177e76376e78c80aa985231
alkotras94 <104847958+alkotras94@users.noreply.github.com>
2024-06-09 10:55:52 +0500

    Save Mac
    Создал 3 скрипта Squad, Unit, Health

---
+++ GameLearnJuno/Assets/Scripts/IE/Health.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int _health;
+
+    public void TakeDamage(int damage)
+    {
+
+    }
+
+    public void AddHealth(int health)
+    {
+
+    }
+
+
+}
---
+++ GameLearnJuno/Assets/Scripts/IE/Squad.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Squad : MonoBehaviour
+{
+    private List<Unit> _units;
+}
