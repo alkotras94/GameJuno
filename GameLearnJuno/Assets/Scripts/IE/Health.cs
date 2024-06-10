fd312f7eebfb123db3bbd6d95889ce55c0b96d91
alkotras94 <104847958+alkotras94@users.noreply.github.com>
2024-06-10 08:01:28 +0500

    Save
    Добавил логику классу Health

--- GameLearnJuno/Assets/Scripts/IE/Health.cs
+++ GameLearnJuno/Assets/Scripts/IE/Health.cs
@@ -1,20 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
+[RequireComponent(typeof(UIHealth))]
 public class Health : MonoBehaviour
 {
     [SerializeField] private int _health;
+    [SerializeField] private int _maxHealth;
+
+    public int HealthSv
+    {
+        get { return _health; }
+    }
+
+    public event UnityAction ChangetHealth;
 
     public void TakeDamage(int damage)
     {
-
+        _health -= damage;
+        ChangetHealth?.Invoke();
+        if (_health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     public void AddHealth(int health)
     {
-
+        if (_health < _maxHealth)
+        {
+            _health += health;
+            if (_health > _maxHealth)
+            {
+                _health = _maxHealth;
+            }
+        }
+        else
+        {
+            _health = _maxHealth;
+        }
     }
-
-
 }
