1901f75ed2a17d28db6f1f6acc5e5f0d0001da9b
alkotras94 <vagin.ewgen@yandex.ru>
2024-08-11 10:00:34 +0500

    ScriptebleObject небольшие добавления

--- GameLearnJuno/Assets/Scripts/ScriptebleObject/ResourcesData.cs
+++ GameLearnJuno/Assets/Scripts/ScriptebleObject/ResourcesData.cs
@@ -2,5 +2,6 @@ using UnityEngine;
 
 public abstract class ResourcesData : ScriptableObject
 {
-    
+    [field: SerializeField] public int TimeToCollect { get; private set; }
+    [field: SerializeField] public int CountResources { get; private set; }
 }
